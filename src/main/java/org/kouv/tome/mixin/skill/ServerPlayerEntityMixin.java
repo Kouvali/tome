8b8a09775606d61564c9c215a10de273e908cb72
Kouvali <230672350+Kouvali@users.noreply.github.com>
2025-09-21 13:27:51 +0900

    Refactor LivingEntityMixin and ServerPlayerEntityMixin

--- src/main/java/org/kouv/tome/mixin/skill/ServerPlayerEntityMixin.java
+++ src/main/java/org/kouv/tome/mixin/skill/ServerPlayerEntityMixin.java
@@ -1,30 +1,31 @@
 package org.kouv.tome.mixin.skill;
 
+import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.World;
 import org.kouv.tome.api.skill.manager.SkillManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ServerPlayerEntity.class)
-public abstract class ServerPlayerEntityMixin {
+public abstract class ServerPlayerEntityMixin extends PlayerEntity {
+    private ServerPlayerEntityMixin(World world, GameProfile profile) {
+        super(world, profile);
+    }
+
     @Inject(method = "onDeath", at = @At(value = "TAIL"))
     private void tome$onDeath(DamageSource damageSource, CallbackInfo ci) {
-        if (tome$serverPlayerEntity().getWorld().isClient()) {
+        if (getWorld().isClient()) {
             return;
         }
 
-        SkillManager skillManager = tome$serverPlayerEntity().getSkillManager();
+        SkillManager skillManager = getSkillManager();
         if (!skillManager.interruptCasting()) {
             skillManager.terminateCasting();
         }
-    }
-
-    @Unique
-    private ServerPlayerEntity tome$serverPlayerEntity() {
-        return (ServerPlayerEntity) (Object) this;
     }
 }
