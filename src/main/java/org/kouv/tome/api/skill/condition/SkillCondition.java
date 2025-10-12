43247f3054f34974272e80899bf92ddcd15825da
Kouvali <230672350+Kouvali@users.noreply.github.com>
2025-10-12 14:32:44 +0900

    Add skill provider parameter to condition methods

--- src/main/java/org/kouv/tome/api/skill/condition/SkillCondition.java
+++ src/main/java/org/kouv/tome/api/skill/condition/SkillCondition.java
@@ -1,23 +1,40 @@
 package org.kouv.tome.api.skill.condition;
 
+import net.minecraft.registry.entry.RegistryEntry;
 import org.jetbrains.annotations.ApiStatus;
+import org.kouv.tome.api.skill.Skill;
 import org.kouv.tome.api.skill.SkillContext;
 import org.kouv.tome.api.skill.SkillResponse;
 
 import java.util.Objects;
+import java.util.function.Function;
 
 @FunctionalInterface
 public interface SkillCondition {
-    static SkillCondition requireLearned() {
-        return context -> context.getSource().getSkillContainer().hasSkill(context.getSkill()) ?
+    static SkillCondition requireLearned(
+            Function<? super SkillContext<?>, ? extends RegistryEntry<? extends Skill<?>>> skillProvider
+    ) {
+        Objects.requireNonNull(skillProvider);
+        return context -> context.getSource().getSkillContainer().hasSkill(skillProvider.apply(context)) ?
                 SkillResponse.success() :
                 SkillResponse.notLearned();
     }
 
-    static SkillCondition requireNoCooldown() {
-        return context -> context.getSource().getSkillCooldownManager().isCoolingDown(context.getSkill()) ?
+    static SkillCondition requireLearned() {
+        return requireLearned(SkillContext::getSkill);
+    }
+
+    static SkillCondition requireNoCooldown(
+            Function<? super SkillContext<?>, ? extends RegistryEntry<? extends Skill<?>>> skillProvider
+    ) {
+        Objects.requireNonNull(skillProvider);
+        return context -> context.getSource().getSkillCooldownManager().isCoolingDown(skillProvider.apply(context)) ?
                 SkillResponse.cooldown() :
                 SkillResponse.success();
+    }
+
+    static SkillCondition requireNoCooldown() {
+        return requireNoCooldown(SkillContext::getSkill);
     }
 
     static SkillCondition requireInGame() {
