b1bd6f6c644319a7dbd4758bad0d3b8d26bbeff6
Kouvali <230672350+Kouvali@users.noreply.github.com>
2025-10-26 22:32:45 +0900

    Add extension functions to Skill.Builder

--- src/main/kotlin/org/kouv/tome/api/skill/action/SkillActionExtensions.kt
+++ src/main/kotlin/org/kouv/tome/api/skill/action/SkillActionExtensions.kt
@@ -5,3 +5,15 @@ import org.kouv.tome.api.skill.SkillResponse
 
 inline fun skillAction(crossinline block: SkillContext.() -> SkillResponse): SkillAction =
     SkillAction { context -> context.block() }
+
+inline fun alwaysSuccessSkillAction(crossinline block: SkillContext.() -> Unit): SkillAction =
+    SkillAction.alwaysSuccess { context -> context.block() }
+
+fun alwaysSuccessSkillAction(): SkillAction = SkillAction.alwaysSuccess()
+
+inline fun alwaysFailureSkillAction(
+    failure: SkillResponse.Failure,
+    crossinline block: SkillContext.() -> Unit
+): SkillAction = SkillAction.alwaysFailure(failure) { context -> context.block() }
+
+fun alwaysFailureSkillAction(failure: SkillResponse.Failure): SkillAction = SkillAction.alwaysFailure(failure)
--- src/main/kotlin/org/kouv/tome/api/skill/condition/SkillConditionExtensions.kt
+++ src/main/kotlin/org/kouv/tome/api/skill/condition/SkillConditionExtensions.kt
@@ -5,3 +5,13 @@ import org.kouv.tome.api.skill.SkillResponse
 
 inline fun skillCondition(crossinline block: SkillContext.() -> SkillResponse): SkillCondition =
     SkillCondition { context -> context.block() }
+
+fun requireNotChannelingSkillCondition(): SkillCondition = SkillCondition.requireNotChanneling()
+
+fun requireLearnedSkillCondition(): SkillCondition = SkillCondition.requireLearned()
+
+fun requireNoCooldownSkillCondition(): SkillCondition = SkillCondition.requireNoCooldown()
+
+fun requireInGameSkillCondition(): SkillCondition = SkillCondition.requireInGame()
+
+fun defaultSkillCondition(): SkillCondition = SkillCondition.defaultConditions()
