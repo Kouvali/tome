76e51e6ba4431b51afd1d2cb2e4ba445c3001a3e
Kouvali <230672350+Kouvali@users.noreply.github.com>
2025-10-28 11:20:23 +0900

    Add default convenience methods to SkillContext

--- src/main/java/org/kouv/tome/api/skill/SkillContext.java
+++ src/main/java/org/kouv/tome/api/skill/SkillContext.java
@@ -2,9 +2,24 @@ package org.kouv.tome.api.skill;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.registry.entry.RegistryEntry;
+import org.kouv.tome.api.skill.manager.SkillContainer;
+import org.kouv.tome.api.skill.manager.SkillCooldownManager;
+import org.kouv.tome.api.skill.manager.SkillManager;
 
 public interface SkillContext<S> {
     RegistryEntry<? extends Skill<S>> getSkill();
 
     LivingEntity getSource();
+
+    default SkillContainer getSkillContainer() {
+        return getSource().getSkillContainer();
+    }
+
+    default SkillCooldownManager getSkillCooldownManager() {
+        return getSource().getSkillCooldownManager();
+    }
+
+    default SkillManager getSkillManager() {
+        return getSource().getSkillManager();
+    }
 }
