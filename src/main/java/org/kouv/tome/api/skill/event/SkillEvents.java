a0c9a9acc823455a208ee93b506ae583fda72713
Kouvali <230672350+Kouvali@users.noreply.github.com>
2025-12-14 19:51:53 +0900

    Remove unnecessary parentheses in SkillEvents class

--- src/main/java/org/kouv/tome/api/skill/event/SkillEvents.java
+++ src/main/java/org/kouv/tome/api/skill/event/SkillEvents.java
@@ -5,31 +5,31 @@ import net.fabricmc.fabric.api.event.EventFactory;
 import org.kouv.tome.api.skill.SkillContext;
 
 public interface SkillEvents {
-    Event<Loaded> LOADED = EventFactory.createArrayBacked(Loaded.class, callbacks -> (context) -> {
+    Event<Loaded> LOADED = EventFactory.createArrayBacked(Loaded.class, callbacks -> context -> {
         for (Loaded callback : callbacks) {
             callback.onLoaded(context);
         }
     });
 
-    Event<Added> ADDED = EventFactory.createArrayBacked(Added.class, callbacks -> (context) -> {
+    Event<Added> ADDED = EventFactory.createArrayBacked(Added.class, callbacks -> context -> {
         for (Added callback : callbacks) {
             callback.onAdded(context);
         }
     });
 
-    Event<Removed> REMOVED = EventFactory.createArrayBacked(Removed.class, callbacks -> (context) -> {
+    Event<Removed> REMOVED = EventFactory.createArrayBacked(Removed.class, callbacks -> context -> {
         for (Removed callback : callbacks) {
             callback.onRemoved(context);
         }
     });
 
-    Event<CooldownStarted> COOLDOWN_STARTED = EventFactory.createArrayBacked(CooldownStarted.class, callbacks -> (context) -> {
+    Event<CooldownStarted> COOLDOWN_STARTED = EventFactory.createArrayBacked(CooldownStarted.class, callbacks -> context -> {
         for (CooldownStarted callback : callbacks) {
             callback.onCooldownStarted(context);
         }
     });
 
-    Event<CooldownEnded> COOLDOWN_ENDED = EventFactory.createArrayBacked(CooldownEnded.class, callbacks -> (context) -> {
+    Event<CooldownEnded> COOLDOWN_ENDED = EventFactory.createArrayBacked(CooldownEnded.class, callbacks -> context -> {
         for (CooldownEnded callback : callbacks) {
             callback.onCooldownEnded(context);
         }
